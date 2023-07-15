743bbdae46b3bd078d193aab439b765289ef8b3e
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-07-14 22:32:23 -0300

    first commit

---
+++ bin/ygg_bot.dart
@@ -0,0 +1,5 @@
+import 'package:ygg_bot/ygg_bot.dart' as ygg_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ygg_bot.calculate()}!');
+}
---
+++ test/ygg_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:ygg_bot/ygg_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
