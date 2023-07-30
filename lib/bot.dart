51fefec5c6df748d1e31ab986fd9dbbc16cf72f1
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-07-30 19:36:07 -0300

    CHANGE main() and yggBot()

--- lib/bot.dart
+++ lib/bot.dart
@@ -1,16 +1,20 @@
 import "package:nyxx/nyxx.dart";
 import "package:ygg_bot/config/config.dart";
 import "package:ygg_bot/models/bot_commands.dart";
+import "package:ygg_bot/models/slash_commands.dart";
 
 void yggBot() {
+  final commands = SlashCommands();
   // Create Nyxx bot instance with necessary intents
   final bot = NyxxFactory.createNyxxWebsocket(
       Config.getDiscordToken(),
       GatewayIntents.allUnprivileged |
           GatewayIntents.allPrivileged |
           GatewayIntents.messageContent)
-    ..registerPlugin(Logging());
+    ..registerPlugin(Logging())
+    ..registerPlugin(commands.commands);
 
+  commands.slashCommands();
   // Listener for when the bot is ready
   bot.eventsWs.onReady.listen((event) {
     print("Ready!");
